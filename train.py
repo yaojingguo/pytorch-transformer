ffcb3f750176398dd82bf7e6c660a829a0a5428a
Jingguo Yao <yaojingguo@gmail.com>
2025-01-01 21:00:53 +0800

    Use local dataset file

--- train.py
+++ train.py
@@ -140,7 +140,8 @@ def get_or_build_tokenizer(config, ds, lang):
 
 def get_ds(config):
     # It only has the train split, so we divide it overselves
-    ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
+    ds_raw = load_dataset('parquet', data_files='train-00000-of-00001.parquet')["train"]
+    # ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
 
     # Build tokenizers
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
